c7834b4d584d1fc33894187b86d3e9042a7397c5
FMuenke <friedrich.muenke@me.com>
2020-07-25 12:01:16 +0200

    new cfg and type fix

--- machine_learning/bag_of_words.py
+++ machine_learning/bag_of_words.py
@@ -107,8 +107,14 @@ class BagOfWords:
     def _bag_up_descriptors(self, descriptors):
         word_bag = np.zeros((1, self.n_words))
         if descriptors is not None:
-            descriptors = descriptors.astype(np.float)
-            words = self.k_means_clustering.predict(descriptors)
+            descriptors = descriptors.astype(np.double)
+            try:
+                words = self.k_means_clustering.predict(descriptors)
+            except Exception as e:
+                print(e)
+                print(descriptors)
+                print(descriptors.shape)
+                words = []
             for word in words:
                 word_bag[0, word] += 1
         return word_bag
