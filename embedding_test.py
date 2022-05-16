be9d24caaa63b518a40bbc7dce56b899b3476dc1
FMuenke <friedrich.muenke@me.com>
2022-05-16 19:36:38 +0200

    image ambedding imrpovements

--- embedding_test.py
+++ embedding_test.py
@@ -1,35 +1,65 @@
-import os
-import cv2
+import pandas as pd
 from classic_image_classification import ImageEmbedding
+import argparse
+import umap
+import numpy as np
+from classic_image_classification import DataSet
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def main():
-    path = "/home/fmuenke/datasets/gashole_data/test/outburst_0"
+    path = "/home/fmuenke/projects/sfm_object_matching/src/OpenSfM/data/reconstruction_project"
     opt = {
         "aggregator": "bag_of_words",
-        "complexity": 32,
-        "feature": "gray-kaze",
-        "sampling_method": "kaze",
+        "complexity": 128,
+        "feature": "hsv-kaze",
+        "sampling_method": "dense",
         "sampling_step": 16,
-        "sampling_window": 8,
+        "sampling_window": 16,
         "image_size": {
-            "width": 128,
-            "height": 128,
+            "width": 64,
+            "height": 64,
         }
     }
     img_emb = ImageEmbedding(opt)
-    img_emb.fit(data_path=path, tag_type="cls")
+    x = img_emb.fit_transform(data_path=path, tag_type="cls")
+    x = np.array(x)
+    x = x[:, 0, :]
+    print(x.shape)
 
-    img_emb.save("/home/fmuenke/ai_models/emb_test")
+    reducer = umap.UMAP(n_components=2)
+    reducer.fit_transform(x)
 
-    img_emb_2 = ImageEmbedding(opt)
-    img_emb_2.load("/home/fmuenke/ai_models/emb_test")
-    for img_f in os.listdir(os.path.join(path, "images")):
-        img = cv2.imread(os.path.join(path, "images",img_f))
-        x = img_emb_2.transform(img)
+    ds = DataSet(data_set_dir=path, tag_type="cls")
+    ds.load_data()
+    tags = ds.get_tags()
+    print(len(tags))
+
+    df = []
+    for t_id in tags:
+        x = img_emb.transform(tags[t_id].load_data())
         print(x)
+        x = reducer.transform(x)
+        print(x)
+        df.append({
+            "name": tags[t_id].tag_class[0],
+            "f1": x[0], "f2": x[1],
+        })
+
+    df = pd.DataFrame(df)
+    sns.scatterplot(data=df, x="f1", y="f2", hue="name")
+    plt.show()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_folder",
+        "-df",
+        default="./data/train",
+        help="Path to directory with predictions",
+    )
 
 if __name__ == "__main__":
     main()
