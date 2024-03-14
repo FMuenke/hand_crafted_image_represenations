280084f7aff3ad8930a18a2959ef0ba2738ceeb7
Friedrich Münke <f.muenke@vialytics.de>
2024-03-14 14:11:32 +0100

    fix

--- handcrafted_image_representations/features/open_cv_descriptor.py
+++ handcrafted_image_representations/features/open_cv_descriptor.py
@@ -3,28 +3,25 @@ import numpy as np
 
 from handcrafted_image_representations.data_structure.image_handler import ImageHandler
 
+pre_built_open_cv_key_point_detectors = {
+    "orb": cv2.ORB_create(),
+    "akaze": cv2.AKAZE_create(),
+    "kaze": cv2.KAZE_create(),
+    "brisk": cv2.BRISK_create(),
+    "sift": cv2.SIFT_create(),
+}
 
 class OpenCvDescriptor:
     def __init__(self, descriptor_type, color_space):
         self.descriptor_type = descriptor_type
         self.color_space = color_space
 
-    def _build_open_cv_descriptor(self):
-        open_cv_key_point_detectors = {
-            "orb": cv2.ORB_create(),
-            "akaze": cv2.AKAZE_create(),
-            "kaze": cv2.KAZE_create(),
-            "brisk": cv2.BRISK_create(),
-            "sift": cv2.SIFT_create(),
-        }
-        return open_cv_key_point_detectors
-
     def _prepare_image(self, image):
         img_h = ImageHandler(image)
         return img_h.prepare_image_for_processing(self.color_space)
 
     def _apply_open_cv_descriptor(self, list_of_targets, open_cv_key_points):
-        open_cv_descriptors = self._build_open_cv_descriptor()
+        open_cv_descriptors = pre_built_open_cv_key_point_detectors
         dc_sets = []
         for target in list_of_targets:
             target = target.astype(np.uint8)
