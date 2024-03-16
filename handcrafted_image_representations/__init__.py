104c8f1cb56b3f365073e6c8a6e1dbe39ec2d53a
Friedrich Münke <f.muenke@vialytics.de>
2024-03-16 17:18:09 +0100

    added import support

--- handcrafted_image_representations/__init__.py
+++ handcrafted_image_representations/__init__.py
@@ -1,5 +1,6 @@
 from handcrafted_image_representations.data_structure.data_set import DataSet  # noqa: F401
 from handcrafted_image_representations.machine_learning import ImageEmbedding  # noqa: F401
+from handcrafted_image_representations.machine_learning import EmbeddingRepository  # noqa: F401
 from handcrafted_image_representations.machine_learning import ImageClassifier  # noqa: F401
 from handcrafted_image_representations.machine_learning import BestOfHandcraftedFeatures  # noqa: F401
 from handcrafted_image_representations.machine_learning import OptimizingImageClassifier  # noqa: F401
